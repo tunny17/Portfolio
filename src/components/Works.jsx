ae6f4fe1d622071111e747426611a2ecbe63f284
tunny17 <tvnji01@gmail.com>
2024-03-13 16:00:52 +0100

    route fix

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,7 +3,7 @@ import { Tilt } from 'react-tilt';
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
-import { github, live } from '/assets';
+import { github, live } from '/public/assets';
 import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
