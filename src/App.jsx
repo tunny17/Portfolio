1a2b083f5a2dbee7d5554e426c29160144d7759c
tunny17 <tvnji01@gmail.com>
2025-01-08 22:44:38 +0100

    feat: performance

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,29 @@ import { BrowserRouter } from 'react-router-dom';
 
 import {
   About,
-  Contact,
+  
   Experience,
-  Hero,
+  
   Navbar,
   Tech,
   Works,
-  StarsCanvas,
 } from './components';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center z-50'>
           <Navbar />
           {/* <Hero /> */}
         </div>
-                  <StarsCanvas />
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
+        {/* <StarsCanvas /> */}
+        <div className='z-50'>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+        </div>
         {/* <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
